8b2c3808ea035830bb339cdc4d60513c7de8201b
Mario Conde <rrsystemsco@gmail.com>
2015-05-13 21:15:01 -0600

    Practica1

---
+++ Ejercicio_14/src/Cuadrado.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Cuadrado {
+
+	public static void main(String [] main){
+		int x=0;
+		String c="*";
+		Scanner in=new Scanner (System.in);
+		
+		
+		System.out.println("El siguiente programa imprime un cuadrado de 'n' elementos ");
+		System.out.println();
+		System.out.println("------------------------------------------------------------");
+		System.out.print("Ingrese una cantidad: ");
+		x=in.nextInt();
+		
+		for (int filas=0; filas<x; filas++){
+			System.out.println(" ");{
+				for (int cols=0; cols<x; cols++){
+					System.out.print("*");
+				}
+		
+		
+			}
+		
+		
+	}
+	
+
+		
+}
+}
