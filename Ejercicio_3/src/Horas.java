6ddb58b8209b9701061ccd24ea8c0ea33ef9b9c8
Mario Conde <rrsystemsco@gmail.com>
2015-05-13 21:04:15 -0600

    Practica1

---
+++ Ejercicio_3/src/Horas.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Horas {
+
+	public static void main (String [] args){
+		
+		int a=0,b=0,c=0,d=0;
+		String e=":";
+		Scanner in=new Scanner (System.in);
+		
+		System.out.println("Cuando usted ingresa una hora en notación de 24 horas la siguiente");
+		System.out.println("aplicación la convierte a notación de 12 horas.");
+		System.out.println();
+		System.out.println();
+		System.out.println("-------------------------------------------------------------------");
+		System.out.print("Ingrese la hora: \r");
+		a=in.nextInt();
+		b=in.nextInt();
+		System.out.println(e);
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+}
